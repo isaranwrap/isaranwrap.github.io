4b450aceacb7b8a3a4d876ffdc10083e8a92d13b
Ishan Saran <ishansaran65@gmail.com>
2023-10-16 20:40:30 -0700

    Adding page & images for Emory Agnition gallery

--- code/circleCursor.js
+++ code/circleCursor.js
@@ -10,3 +10,22 @@ document.addEventListener('DOMContentLoaded', function () {
 			// document.removeEventListener('mousemove', this);
 		  });
 		});
+
+// Circle cursor style (sometimes this is duplicated (read: unnecessary))
+var styles = `
+    .circle-cursor {
+    position: fixed;
+    width: 10px;
+    height: 10px;
+    /* background-color: #008ECE; */
+    background-color: rgb(135, 206, 235);
+    border-radius: 50%;
+    pointer-events: auto;
+    z-index: 9999;
+    display: none;
+}
+`
+
+var styleSheet = document.createElement("style")
+styleSheet.textContent = styles
+document.head.appendChild(styleSheet)
